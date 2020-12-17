eba967a84c56e98ac306fc036775ecf37c587c5d
b07602037 <75796070+b07602037@users.noreply.github.com>
2020-12-17 17:49:43 +0800

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,15 +1,27 @@
 import urllib.request as req
-url = "https://oia.ntu.edu.tw/ch/outgoing/view/sn/1115"
+url = "https://oia.ntu.edu.tw/ch/outgoing/school.list/country_sn/0"
 with req.urlopen(url) as response:
     data = response.read().decode("utf-8")
 
 import bs4
 root = bs4.BeautifulSoup(data, "html.parser")
-school_name = root.find("h1")
-titles = root.find_all("th")
-infos = root.find_all("td")
-print(school_name.text)
-for title in titles:
-    print(title.text)
-for info in infos:
-    print(info.text)
+urls = root.find_all("td", class_="_decoration")
+for url in urls:
+    url = "https://oia.ntu.edu.tw"+url.a["href"]
+    with req.urlopen(url) as response:
+        data = response.read().decode("utf-8")
+    root = bs4.BeautifulSoup(data, "html.parser")
+    school_name = root.find("h1")
+    qualifications = root.find("h3")
+    titles = root.find_all("th")
+    infos = root.find_all("td")
+    print(school_name.text)
+    print(len(titles))
+    print(len(infos))
+    print(titles[0].text)
+    print(qualifications.text)
+    for i in range(1, len(infos)):
+    #for title in titles:
+        print(titles[i].text)
+    #for info in infos:
+        print("index:", i, infos[i].text)
