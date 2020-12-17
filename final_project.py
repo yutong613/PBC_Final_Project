1f5fb474997c9624962a12f6c4fd8e1395bd0447
b07602037 <75796070+b07602037@users.noreply.github.com>
2020-12-17 22:09:41 +0800

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -6,22 +6,19 @@ with req.urlopen(url) as response:
 import bs4
 root = bs4.BeautifulSoup(data, "html.parser")
 urls = root.find_all("td", class_="_decoration")
+
 for url in urls:
     url = "https://oia.ntu.edu.tw"+url.a["href"]
     with req.urlopen(url) as response:
         data = response.read().decode("utf-8")
     root = bs4.BeautifulSoup(data, "html.parser")
     school_name = root.find("h1")
-    qualifications = root.find("h3")
     titles = root.find_all("th")
     infos = root.find_all("td")
+    print("\n")
     print(school_name.text)
-    print(len(titles))
-    print(len(infos))
-    print(titles[0].text)
-    print(qualifications.text)
-    for i in range(1, len(infos)):
-    #for title in titles:
-        print(titles[i].text)
-    #for info in infos:
-        print("index:", i, infos[i].text)
+    for i in range(len(titles)):
+        if i != 0:
+            print(titles[i].text, infos[i+len(infos)-len(titles)].text.strip())
+        else:
+            print(titles[i].text, infos[i].text.strip())
